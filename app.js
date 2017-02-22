458656b6b7d989a3fe3ceb29a4a44c0a4ce3947b
Juan Sotomayor Rpi <juan.sotomayor.ch@gmail.com>
2017-02-22 02:03:20 -0300

    Archivos bases

---
+++ app.js
@@ -0,0 +1,138 @@
+// app.js
+var express = require('express');  
+var app = express();  
+var server = require('http').createServer(app);  
+var io = require('socket.io')(server);
+var serialport = require('serialport');
+var SerialPort = serialport.SerialPort;
+
+var port = new SerialPort('/dev/ttyACM0',{
+   baudRate:9600,
+   parser: serialport.parsers.readline('\r\n')
+});
+
+var DataArduino = "";
+var led_state = "LOW";
+var ventilacion = "LOW";
+//var State = "Growth";
+var State = "Blooming";
+var receivedData = "";
+var DataString = "";
+var temperatura, humedad, LDR_inferior, LDR_superior, pH, ventilador_derecho, ventilador_izquierdo = 0;
+
+port.on('open', function(){
+    console.log('Serial Port Opend');
+});
+
+port.on('data', function(data){
+    DataArduino = DataArduino + data;
+    var posicion = DataArduino.lastIndexOf('*');
+    if(posicion > 0){
+        console.log(DataArduino.substring(0, posicion));
+        Controlador(DataArduino.substring(0, posicion));
+        DataArduino = DataArduino.substring(posicion+1)
+    }
+});
+
+port.on('close', function(){
+    console.log("puerto cerrado... reconectando");
+    port = new SerialPort('/dev/ttyACM0');
+});
+
+
+app.use(express.static(__dirname + '/node_modules'));  
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res,next) {  
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', function(client) {  
+    var socketId = socket.id;
+    var clientIp = socket.request.connection.remoteAddress;
+
+    console.log('Cliente ip '+clientIp);
+    console.log('connection :', socket.request.connection._peername);
+
+    client.on('join', function(data) {
+        console.log(data);
+    });
+
+    client.on('messages', function(data) {
+           console.log(data);
+           client.emit('broad', data);
+           client.broadcast.emit('broad',data);
+    });
+
+});
+
+server.listen(4200);  
+
+function Controlador(data){
+    var receivedData = data.toString();
+    var Data = receivedData.split("-");
+    dataSize = Data.length;
+    console.log(Data);
+    temperatura = Data[0];
+    humedad = Data[1];
+    pH = Data[2];
+    LDR_inferior = Data[3];
+    LDR_superior = Data[4];
+    ventilador_derecho = Data[5];
+    ventilador_izquierdo = Data[6];
+    io.sockets.emit('temperatura', temperatura);
+    io.sockets.emit('humedad',  humedad);
+    io.sockets.emit('pH', pH);
+    io.sockets.emit('LDR_inferior', LDR_inferior);
+    io.sockets.emit('LDR_superior',  LDR_superior);
+    io.sockets.emit('ventilador_derecho',ventilador_derecho);
+    io.sockets.emit('ventilador_izquierdo', ventilador_izquierdo);
+
+}
+setInterval(function() {
+  var date = new Date();
+  var current_hour = date.getHours();
+  var current_minute = date.getMinutes();
+  console.log("Hora: "+current_hour+"minuto = " + current_minute);
+  console.log("LDR sup => "+LDR_superior);
+
+  if(LDR_superior > 500 && (current_hour >= 16 && current_hour < 19)){
+
+    led_state = "LOW";
+     console.log("LED STATE LOW");
+    Panel_led(led_state, State);
+  }else if(LDR_superior < 100 && (current_hour < 16 ||  current_hour >= 19)) {
+    led_state = "HIGH";
+    console.log("LED STATE HIGH");
+    Panel_led(led_state, State);
+  }
+
+  if(ventilador_derecho == "0" && ((current_minute >= 0 && current_minute < 15) || (current_minute >= 30 &&$
+    ventilacion = "HIGH";
+    console.log("Ventilador HIGH");
+    Sistema_ventilacion(ventilacion);
+  }else if(ventilador_derecho == "1" && ((current_minute >= 15 && current_minute < 30) || (current_minute >$
+    ventilacion = "LOW";
+     console.log("Ventilador LOW");
+    Sistema_ventilacion(ventilacion);
+  }
+
+
+
+}, 3000);
+
+function Panel_led(led, State){
+
+  port.write("*"+State+"*");
+  port.write(led);
+  port.write("-");
+  console.log("*"+led+"-");
+}
+
+function Sistema_ventilacion(ventilacion){
+  port.write("*Ventilacion*");
+  port.write(ventilacion);
+  port.write("-");
+
+}
+
